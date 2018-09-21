72e2e48f6f00c24b808865dd605597aa35c1e69e
codeauslander <cesar.gusto.catano@gmail.com>
2018-09-20 20:57:46 -0500

    Added signup page inputs

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -17,6 +17,21 @@ class Signup extends React.Component {
     email: '',
     password: '',
     confirmation: '',
+    zipcode: '',
+
+    pet_name: '',
+    pet_bio: '',
+    start_date: '',
+    end_date: '',
+    petowner_image_file_name: '',
+    // petowner_image_content_type: '',
+    // petowner_image_file_size: '',
+
+    bio: '',
+    sitter_image_file_name: '',
+    // sitter_image_content_type: '',
+    // sitter_image_file_size: '',
+    
     type: 'sitter',
   }
 
@@ -57,8 +72,89 @@ class Signup extends React.Component {
     const { name, value } = event.target;
     this.setState({[name]: value});
   }
+
+
  
   render() {
+
+    const { type } = this.state
+
+    const form_petowner = <React.Fragment>
+      <li>
+        <label htmlFor="pet_name">Pet Name</label>
+        <input 
+          name='pet_name' 
+          value={this.state.pet_name} 
+          type="text" id="pet_name" 
+          placeholder="Pet name." 
+          onChange={this.handleChange}
+        />
+      </li>
+      <li>
+        <label htmlFor="pet_bio">Pet bio</label>
+        <input 
+          name='pet_bio' 
+          value={this.state.pet_bio} 
+          type="text" id="pet_bio" 
+          placeholder="Pet bio." 
+          onChange={this.handleChange}
+        />
+      </li>
+      <li>
+        <label htmlFor="start_date">Start Date</label>
+        <input 
+          name='start_date' 
+          value={this.state.start_date} 
+          type="text" id="start_date" 
+          placeholder="Start date, When the sitter starts to take care of your pet." 
+          onChange={this.handleChange}
+        />
+      </li>
+      <li>
+        <label htmlFor="end_date">end Date</label>
+        <input 
+          name='end_date' 
+          value={this.state.end_date} 
+          type="text" id="end_date" 
+          placeholder="End date, When the sitter return the pet to the owner." 
+          onChange={this.handleChange}
+        />
+      </li>
+      <li>
+        <label htmlFor="petowner_image_file_name">Dog image</label>
+        <input 
+          name='petowner_image_file_name' 
+          value={this.state.petowner_image_file_name} 
+          type="file" id="petowner_image_file_name" 
+          placeholder="Pick up the image for your pet from your machine." 
+          onChange={this.handleChange}
+        />
+      </li>
+    </React.Fragment>
+      
+    const form_sitter = <React.Fragment>
+      <li>
+        <label htmlFor="bio">Bio</label>
+        <input 
+          name='bio' 
+          value={this.state.bio} 
+          id="bio" 
+          placeholder="Describe Yourself as a guardian" 
+          onChange={this.handleChange}
+        />
+      </li>
+      <li>
+        <label htmlFor="sitter_image_file_name">Profile image</label>
+        <input 
+          name='sitter_image_file_name' 
+          value={this.state.sitter_image_file_name} 
+          type="file" id="sitter_image_file_name" 
+          placeholder="Upload an image of You" 
+          onChange={this.handleChange}
+        />
+      </li>
+    </React.Fragment>
+
     return (
       <form className='signup' onSubmit={this.handleSubmit}>
         <ul className="flex-outer">
@@ -95,11 +191,22 @@ class Signup extends React.Component {
             />
           </li>
           <li>
+            <label htmlFor="zipcode">Zipcode</label>
+            <input 
+              name='zipcode' 
+              value={this.state.zipcode} 
+              type="text" id="zipcode" 
+              placeholder="Zipcode" 
+              onChange={this.handleChange}
+            />
+          </li>
+          <li>
             <fieldset>
               <legend>User</legend>
               <ul className="flex-inner">
                 <li>
                   <input 
+                    defaultChecked
                     name='type' 
                     value='sitter'
                     type="radio" 
@@ -121,6 +228,9 @@ class Signup extends React.Component {
               </ul>
             </fieldset>
           </li>
+          
+          { type === 'owner' ? form_petowner : form_sitter }
+          
           <li>
             <button type="submit">Submit</button>
           </li>
